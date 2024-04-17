d345205943254187a3b7b93b7af54784a14fe0b8
MarkHenyecz <business@henyeczmark.hu>
2024-04-17 22:20:22 +0100

    Default admin panel integration

---
+++ app/Filament/Resources/CharacterResource.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CharacterResource\Pages;
+use App\Filament\Resources\CharacterResource\RelationManagers;
+use App\Models\Character;
+use App\Models\File;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\MorphToSelect;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CharacterResource extends Resource
+{
+    protected static ?string $model = Character::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('user_id')
+                    ->relationship('user', 'name'),
+                TextInput::make('name'),
+                
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('user.name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCharacters::route('/'),
+            'create' => Pages\CreateCharacter::route('/create'),
+            'edit' => Pages\EditCharacter::route('/{record}/edit'),
+        ];
+    }    
+}
